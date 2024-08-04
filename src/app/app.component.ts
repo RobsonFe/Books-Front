771445ba110e75efedfb44c892fd68bc766b876b
RobsonFe <robson_20ferreira@hotmail.com>
2024-08-04 19:46:02 -0300

    feat:footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './modules/components/navbar/navbar.component';
 import { ProfileComponent } from './modules/components/profile/profile.component';
 import { initFlowbite } from 'flowbite';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
