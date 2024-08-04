09476189a63df372921fe46a8d993fe8de7ecea7
RobsonFe <robson_20ferreira@hotmail.com>
2024-08-04 00:41:03 -0300

    Repositoio semanal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './modules/components/navbar/navbar.component';
+import { ProfileComponent } from './modules/components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'profile', component: ProfileComponent },
+];
