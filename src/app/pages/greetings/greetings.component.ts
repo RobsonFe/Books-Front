8a510697b0f607ade06800761997c260f09acb17
RobsonFe <robson_20ferreira@hotmail.com>
2024-08-05 21:27:43 -0300

    feat:carousel

--- src/app/pages/greetings/greetings.component.ts
+++ src/app/pages/greetings/greetings.component.ts
@@ -1,10 +1,11 @@
+import { CarouselComponent } from '@/app/modules/components/carousel/carousel.component';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-greetings',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CarouselComponent],
   templateUrl: './greetings.component.html',
   styleUrl: './greetings.component.css',
 })
