f20bc37a0b73b52b9c57908ba86c111ce0eb4755
RobsonFe <robson_20ferreira@hotmail.com>
2024-08-11 01:24:56 -0300

    featValidationPassword

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,11 +1,50 @@
 import { HomeComponent } from '@/app/home/home.component';
-import { Component } from '@angular/core';
+import { User } from '@/app/modules/model/user.model';
+import { UserService } from '@/app/modules/service/user.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, CommonModule, FormsModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css',
+  styleUrls: ['./form.component.css'],
 })
-export class FormComponent {}
+export class FormComponent implements OnInit {
+  newUser: User = {
+    id: 0,
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {}
+
+  createUser(): void {
+    console.log('Dados do Usuário:', this.newUser);
+    if (this.newUser.password === this.newUser.confirmPassword) {
+      console.log(
+        `Conta Criada com Sucesso! Nome: ${this.newUser.name}, Email: ${this.newUser.email}`
+      );
+      this.limpar();
+    } else {
+      console.log('As senhas não são iguais');
+      this.limpar();
+    }
+  }
+
+  limpar() {
+    this.newUser = {
+      id: 0,
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+}
